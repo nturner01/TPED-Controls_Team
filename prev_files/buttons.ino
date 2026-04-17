a04ce9709a1ba4b19699d62479b4006233a44bab
ehoenig <ehoenig@binghamton.edu>
2026-04-17 14:18:15 -0400

    Update buttons.ino

--- prev_files/buttons.ino
+++ prev_files/buttons.ino
@@ -1,31 +1,20 @@
-// Feel free to rename these to make your code more readable! Leave the values to 0, we'll assign these later
-const int pin1 = 0;
-const int pin2 = 0;
+const int pinDispatchInput = 0;
+const int pinDispatchLight = 0;
 
 void setup(){
-    /*
-    If you have an LED button:
-    pinMode(pin1, INPUT_PULLUP); --> sets up reading the pressing of the button
-    pinMode(pin2, OUTPUT); --> sets up lighting up the LED
-
-    If you have a toggle button:
-    pinMode(pin1, INPUT_PULLUP); -->  sets up reading the top beam
-    pinMode(pin2, INPUT_PULLUP); -->  sets up reading the botton beam
-    */
-   Serial.begin(9600);
+    pinMode(pinDispatchInput, INPUT_PULLUP);
+    pinMode(pinDispatchLight, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop(){
-    /* Add whatever logic here you need to 
-    1. Read the input of the button (maybe make it print something using Serial.println())
-    2. Either read the second input (if you have a toggle) or manipulate the LED (if you have an LED button)
+    // Only bother checking for inputs if the ride can actually be dispatched
+    if (digitalRead(pinDispatchInput) == LOW /* && state == idleReady */) {
+        //set state to dispatched
+    }
 
-    Helpful functions:
-    digitalRead(pin) --> returns the reading of the specified pin (LOW or HIGH).
-        **NOTE: INPUT_PULLUP MEANS THAT A READING OF LOW MEANS THE BUTTON IS PRESSED, AND HIGH MEANS IT IS NOT!!!!!**
-    digitalWrite(pin, VALUE) --> sets the pin to the VALUE specified, use either HIGH or LOW (use this for the LED)
-
-    Note: If you have a pushbutton, you need to make sure your code recognizes when the button actually goes from not pressed to pressed, not just being held down!
-    */
+    // Have the light visually indicate that the button only does anything if the ride is ready to dispatch
+    if (/*state == idleReady*/) {digitalWrite(pinDispatchLight, HIGH)}
+    else                        {digitalWrite(pinDispatchLight, LOW )}
 }
 
