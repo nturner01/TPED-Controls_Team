5f3a6027afa5398e69b52152ea726ddbeee37484
celesteoyeku <157048321+celesteoyeku@users.noreply.github.com>
2026-04-17 15:28:12 -0400

    drive direction mode code

--- buttons.ino
+++ buttons.ino
@@ -1,31 +1,44 @@
-// Feel free to rename these to make your code more readable! Leave the values to 0, we'll assign these later
-const int pin1 = 0;
-const int pin2 = 0;
+
+
+const int pin1 = 2; // forward
+const int pin2 = 3; // reverse
+
+
+int lastForward = HIGH;
+int lastReverse = HIGH;
 
 void setup(){
-    /*
-    If you have an LED button:
-    pinMode(pin1, INPUT_PULLUP); --> sets up reading the pressing of the button
-    pinMode(pin2, OUTPUT); --> sets up lighting up the LED
-
-    If you have a toggle button:
-    pinMode(pin1, INPUT_PULLUP); -->  sets up reading the top beam
-    pinMode(pin2, INPUT_PULLUP); -->  sets up reading the botton beam
-    */
-   Serial.begin(9600);
+    pinMode(pin1, INPUT_PULLUP);
+    pinMode(pin2, INPUT_PULLUP);
+    Serial.begin(9600);
 }
 
 void loop(){
-    /* Add whatever logic here you need to 
-    1. Read the input of the button (maybe make it print something using Serial.println())
-    2. Either read the second input (if you have a toggle) or manipulate the LED (if you have an LED button)
+    int forwardState = digitalRead(pin1);
+    int reverseState = digitalRead(pin2);
 
-    Helpful functions:
-    digitalRead(pin) --> returns the reading of the specified pin (LOW or HIGH).
-        **NOTE: INPUT_PULLUP MEANS THAT A READING OF LOW MEANS THE BUTTON IS PRESSED, AND HIGH MEANS IT IS NOT!!!!!**
-    digitalWrite(pin, VALUE) --> sets the pin to the VALUE specified, use either HIGH or LOW (use this for the LED)
+    
+    if (lastForward == HIGH && forwardState == LOW) {
+        Serial.println("DISPATCH / FORWARD");
+    }
 
-    Note: If you have a pushbutton, you need to make sure your code recognizes when the button actually goes from not pressed to pressed, not just being held down!
-    */
+
+    if (lastReverse == HIGH && reverseState == LOW) {
+        Serial.println("REVERSE");
+    }
+
+
+    if (forwardState == HIGH && reverseState == HIGH &&
+        (lastForward == LOW || lastReverse == LOW)) {
+        Serial.println("NEUTRAL / IDLE");
+    }
+
+   
+    if (forwardState == LOW && reverseState == LOW) {
+        Serial.println("ERROR: invalid switch state");
+    }
+
+   
+    lastForward = forwardState;
+    lastReverse = reverseState;
 }
-
