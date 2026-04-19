277b187b9afe9b7bb72461f4f313b45ca2fe5613
zeppb-63 <zbilgehan@binghamton.edu>
2026-04-19 11:33:45 -0400

    Refactor LED class with button state and initialization
    Refactor LED class to include button handling and improve structure. Trouble Button

--- master/led.cpp
+++ master/led.cpp
@@ -1,20 +1,49 @@
 #include <Arduino.h>
 
-class LED{
+class LED {
 private:
-int buttonPin;
-int ledPin;
-unsigned long prevTime;
+    int ledPin;
+    unsigned long prevTime;
+    bool ledState;
 
 public:
-void blink(int interval){
-    unsigned long currTime = millis();
-    if(currTime - prevTime >= interval){
-        prevTime = currTime;
-        digitalWrite(ledPin, !digitalRead(ledPin));
-    }
-}
-LED(int buttonPin , int ledPin) : buttonPin(buttonPin), ledPin(ledPin), prevTime(0) {}
-};
+    LED(int ledPin) : ledPin(ledPin), prevTime(0), ledState(false) {}
 
-LED estop();
+    void begin() {
+        pinMode(ledPin, OUTPUT);
+        digitalWrite(ledPin, LOW);
+    }
+
+    // buttonState: LOW -> pressed, HIGH -> not pressed
+    void update(int buttonState, unsigned long interval = 500) {
+        // if the button is pressed, turn off LED
+        if (buttonState == LOW) {
+            ledState = false;
+            digitalWrite(ledPin, LOW);
+            return;
+        }
+
+        // if the button isn't pressed, blink
+        unsigned long currTime = millis();
+        if (currTime - prevTime >= interval) {
+            prevTime = currTime;
+            ledState = !ledState;
+            digitalWrite(ledPin, ledState ? HIGH : LOW);
+        }
+    }
+};
+// 13, 2 ???
+
+LED estop(13);
+
+const int buttonPin = 2;
+
+void setup() {
+    pinMode(buttonPin, INPUT_PULLUP); 
+    estop.begin();
+}
+
+void loop() {
+    int buttonState = digitalRead(buttonPin); 
+    estop.update(buttonState, 500);
+}
